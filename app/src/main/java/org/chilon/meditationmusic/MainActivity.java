a58af0e03ea9225134d5605e269a56247f851881
pawchi <pawchi@o2.pl>
2018-06-06 16:09:36 +0200

    Linear Layout

--- app/src/main/java/org/chilon/meditationmusic/MainActivity.java
+++ app/src/main/java/org/chilon/meditationmusic/MainActivity.java
@@ -2,12 +2,21 @@ package org.chilon.meditationmusic;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+    ListView lst;
+    String[] musicType = {"Lake","Monk","Flower"};
+    String[] musicDescription = {"This is Lake","This is Monk","this is Flower"};
+    Integer[] image = {R.drawable.lake_small_squere,R.drawable.monk_smal_squerel,R.drawable.flower_small_squere};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lst = (ListView) findViewById(R.id.listview);
+        CustomListView customListView=new CustomListView(this,musicType,musicDescription,image);
+        lst.setAdapter(customListView);
     }
 }
