c090c0f640c535ffac61cc0fbd2494f2322a1fe6
pawchi <pawchi@o2.pl>
2018-08-06 16:31:49 +0200

    add_sounds_seekbars

--- app/src/main/java/org/chilon/meditationmusic/AddMusic.java
+++ app/src/main/java/org/chilon/meditationmusic/AddMusic.java
@@ -2,14 +2,20 @@ package org.chilon.meditationmusic;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class AddMusic extends Activity {
+    PerfectLoopMediaPlayer music_1;
+    SeekBar seekBar_1;
+    SeekBar seekBar_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_music);
         this.setFinishOnTouchOutside(false);
+
 
 
     }
