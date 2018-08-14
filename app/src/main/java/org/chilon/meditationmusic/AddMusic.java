880243dcde952a549c3da9348851a6ec772cfe87
pawchi <pawchi@o2.pl>
2018-08-14 15:26:34 +0200

    cancel_dynamic elements from XML

--- app/src/main/java/org/chilon/meditationmusic/AddMusic.java
+++ app/src/main/java/org/chilon/meditationmusic/AddMusic.java
@@ -20,9 +20,18 @@ public class AddMusic extends Activity {
     LinearLayout existingLayoutForInserts;
     PerfectLoopMediaPlayer soundOne;
     PerfectLoopMediaPlayer soundTwo;
+    final int soundOneId = R.id.sound_1_to_add_imageview;
+    final int soundTwoId = R.id.sound_2_to_add_imageview;
+    int cancelOneId;
+    int cancelTwoId;
+    Integer tagOne = 1111;
+    Integer tagTwo = 2222;
     ImageView cancelSoundOne;
+    ImageView cancelSoundTwo;
     View sound_1;
     boolean view_1_inflated = false;
+    Integer tempIdCancelViewOne;
+    Integer tempIdCancelViewTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +40,14 @@ public class AddMusic extends Activity {
         this.setFinishOnTouchOutside(false);
 
         existingLayoutForInserts = (LinearLayout) findViewById(R.id.linear_layout_for_inserts);
-
+        /*
         firstSoundToAddImage = (ImageView) findViewById(R.id.sound_1_to_add_imageview);
         firstSoundToAddImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 LayoutInflater rowInLinearLayoutToInflate = (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                existingLayoutForInserts = (LinearLayout) findViewById(R.id.linear_layout_for_inserts);
+                //existingLayoutForInserts = (LinearLayout) findViewById(R.id.linear_layout_for_inserts);
 
                 sound_1 = rowInLinearLayoutToInflate.inflate(R.layout.linearlayout_add_sound,existingLayoutForInserts,false);
                 existingLayoutForInserts.addView(sound_1);
@@ -47,23 +56,6 @@ public class AddMusic extends Activity {
                 view_1_inflated=true;
             }
         });
-
-
-            cancelSoundOne = (ImageView) findViewById(R.id.music_1_cancel__from_xml);
-            if(cancelSoundOne!=null) {
-
-                cancelSoundOne.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        soundTwo.pause();
-                        existingLayoutForInserts.removeView(sound_1);
-                    }
-                });
-            }
-
-
-
-
 
         secondSoundToAddImage = (ImageView) findViewById(R.id.sound_2_to_add_imageview);
 
@@ -94,5 +86,49 @@ public class AddMusic extends Activity {
 
             }
         });
+        */
+    }
+
+    public void onDelete(View view){
+
+
+        if (view.getId()==cancelSoundOne.getId()){
+            soundOne.pause();
+        }
+/*
+        if(view.getId()==cancelSoundTwo.getId()){
+            soundTwo.pause();
+        }
+*/
+        existingLayoutForInserts.removeView((View) view.getParent());
+    }
+    public void addSound(View view){
+        int tempId = view.getId();
+        LayoutInflater rowInLinearLayoutToInflate = (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        sound_1 = rowInLinearLayoutToInflate.inflate(R.layout.linearlayout_add_sound,existingLayoutForInserts,false);
+        existingLayoutForInserts.addView(sound_1);
+
+        cancelSoundOne = (ImageView) findViewById(R.id.music_1_cancel__from_xml);
+
+        switch (tempId){
+            case soundOneId:
+                tempIdCancelViewOne = View.generateViewId();
+                cancelSoundOne.setId(tempIdCancelViewOne);
+                soundOne = PerfectLoopMediaPlayer.create(AddMusic.this,R.raw.water);
+                soundOne.prepare();
+
+                break;
+/*
+            case soundTwoId:
+                //cancelSoundTwo = (ImageView) findViewById(R.id.music_1_cancel__from_xml);
+                //cancelSoundTwo.setId(View.generateViewId());
+                tempIdCancelViewTwo = View.generateViewId();
+                cancelSoundTwo.setId(tempIdCancelViewTwo);
+                soundTwo = PerfectLoopMediaPlayer.create(AddMusic.this,R.raw.korg);
+                soundTwo.prepare();
+
+                break;
+*/
+        }
     }
 }
